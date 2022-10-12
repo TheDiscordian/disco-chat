476e52d4ab312cd2995be18f42ae8b6bfacfaa24
Discordian <therealdiscordian@gmail.com>
2022-10-12 02:32:58 -0400

    Set window boundaries & remove forgotted line

--- src/main.rs
+++ src/main.rs
@@ -36,7 +36,6 @@ async fn main() {
     }
   });
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 
