c70e2f5381761edd2710c081a917c395b3aabe85
Discordian <therealdiscordian@gmail.com>
2023-01-23 16:46:30 -0500

    ipfs-http-client -> kubo-rpc-client

--- ui/lib.js
+++ ui/lib.js
@@ -17,5 +17,5 @@ async function INIT_IPFS() {
 	IPFS_API_PORT = await __TAURI__.invoke('get_api_port');
 	IPFS_SWARM_PORT = await __TAURI__.invoke('get_swarm_port');
 
-	ipfs = await IpfsHttpClient.create({url: "http://127.0.0.1:"+IPFS_API_PORT.toString(), timeout: "5m"});
+	ipfs = await KuboRpcClient.create({url: "http://127.0.0.1:"+IPFS_API_PORT.toString(), timeout: "5m"});
 }
